4033f1d88e2a22fd497a9f2892f7fb6c277efd14
Venkata Sudhakar <venkatasudhakar33@gmail.com>
2020-05-08 15:27:25 +0530

    first commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by venkata sudhakar on 08/05/20.
+//  Copyright © 2020 venkata sudhakar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
