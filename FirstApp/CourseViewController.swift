8e5af1914536d3857256b4f4627c98d9fac5f3c4
Venkata Sudhakar <venkatasudhakar33@gmail.com>
2020-05-09 16:54:55 +0530

    course view with basic set up

---
+++ FirstApp/CourseViewController.swift
@@ -0,0 +1,51 @@
+//
+//  CourseViewController.swift
+//  FirstApp
+//
+//  Created by venkata sudhakar on 09/05/20.
+//  Copyright © 2020 venkata sudhakar. All rights reserved.
+//
+
+import UIKit
+
+class CourseViewController: UIViewController {
+    let basic = ["variables"]
+    let advanced = ["maps"]
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    @IBOutlet weak var courseTable: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        courseTable.dataSource = self
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func actionForSegment(_ sender: Any) {
+    }
+}
+
+extension CourseViewController:UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.segment.selectedSegmentIndex == 0 {
+            return basic.count
+        }else{
+            return advanced.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        if self.segment.selectedSegmentIndex == 0 {
+            cell.textLabel?.text = basic[indexPath.row]
+        }
+        else{
+            cell.textLabel?.text = advanced[indexPath.row]
+        }
+        return cell
+    }
+    
+    
+    
+    
+    
+    
+}
